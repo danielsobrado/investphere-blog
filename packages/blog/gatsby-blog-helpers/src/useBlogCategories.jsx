15703426cde87a3ec5a201b6e6c6d152ce8bee31
Daniel Sobrado <35833752+danielsobrado@users.noreply.github.com>
2023-06-27 16:11:38 +0400

    rollback reverse in useBlogCategories

--- packages/blog/gatsby-blog-helpers/src/useBlogCategories.jsx
+++ packages/blog/gatsby-blog-helpers/src/useBlogCategories.jsx
@@ -3,15 +3,9 @@ import dedupe from 'dedupe'
 
 export const useBlogCategories = () => {
   const { allArticleCategory } = useStaticQuery(categoriesQuery)
-
-  // Check if categories are available
-  if (!allArticleCategory.nodes) return null;
-
-  // Remove duplicates and sort categories in descending order
-  const dedupedAndSortedCategories = dedupe(allArticleCategory.nodes, node => node.slug)
-    .sort((a, b) => b.name.localeCompare(a.name));
-
-  return dedupedAndSortedCategories;
+  return allArticleCategory.nodes
+    ? dedupe(allArticleCategory.nodes, node => node.slug)
+    : null
 }
 
 const categoriesQuery = graphql`
